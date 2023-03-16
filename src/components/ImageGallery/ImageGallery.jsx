6d19d6d1d7e2de8d1fc8ef5fcf6b88c7f4ead90f
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2023-03-17 00:11:42 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem'
 import PropTypes from 'prop-types'; 
 
 import css from 'components/ImageGallery/ImageGallery.module.css'
 
 
-class ImageGallery extends Component {
- 
-
-    render () {
-     const imagesArray = this.props;
-
+function ImageGallery({onClick, renderArray}) {
         return (
         <ul className={css.imageGallery}>
-            { (imagesArray.renderArray !== null) &&
+            { (renderArray !== null) &&
             
             <><ImageGalleryItem 
-            imageArray={imagesArray.renderArray} 
-            onClick={imagesArray.onClick}/>
+            imageArray={renderArray} 
+            onClick={onClick}/>
            </>     
         } 
       </ul>
-
         )
-    }
 }
 
 ImageGallery.propTypes = {
